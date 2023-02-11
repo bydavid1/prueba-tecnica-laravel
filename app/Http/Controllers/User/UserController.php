21602c30935299f927dcaee5ac27ba98529913a4
Byron Jimenez <byronjimenez9911@gmail.com>
2023-02-10 20:34:15 -0800

    Added: Delete user feature

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function listUsers() {
-        $users = User::all();
+        $users = User::where('id', '!=', Auth::user()->id)->get();
         // dd($users);
 
         return view('dashboard', ['users' => $users]);
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -29,6 +29,11 @@
                                     <td class="border px-4 py-2">{{ $user->email_verified_at }}</td>
                                     <td class="border px-4 py-2">
                                         <a href="{{ route('user.edit', $user->id) }}" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Editar</a>
+                                        <form action="{{ route('user.destroy', $user->id) }}" method="POST" class="inline">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button type="submit" class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">Eliminar</button>
+                                        </form>
                                     </td>
                                 </tr>
                             @empty
